d38675f549d3967abb9f737aa8408c59bc217b19
Zealot <yizhou.tao@yinyuetai.com>
2018-02-09 16:10:44 +0800

    df

--- src/fanstop/rfm/preprocess/FanstopPreprocess.scala
+++ src/fanstop/rfm/preprocess/FanstopPreprocess.scala
@@ -11,6 +11,8 @@ package fanstop.rfm.preprocess
 import java.text.{DecimalFormat, SimpleDateFormat}
 import java.util.Date
 
+import org.apache.spark.ml.feature.QuantileDiscretizer
+import org.apache.spark.sql.SQLContext
 import org.apache.spark.{SparkContext, SparkConf}
 import org.apache.spark.mllib.clustering.{KMeans, KMeansModel}
 import org.apache.spark.mllib.linalg.Vectors
@@ -32,6 +34,7 @@ object FanstopPreprocess {
     var hehe = dateFormat.format( now )
     hehe
   }
+  case class RFM(r:Int,f:Int,m:Int)
 
   //核心工作时间，迟到早退等的的处理
   def getCoreTime(start_time: String, end_Time: String) = {
@@ -51,7 +54,8 @@ object FanstopPreprocess {
 //做一个过滤，粉丝数》50，标签保留人数的top50
     val sparkConf = new SparkConf().setAppName("FanstopPreprocess yizhou").setMaster("local[1]")
     val sc = new SparkContext(sparkConf)
-
+    val sqlCon=new SQLContext(sc)
+    import sqlCon.implicits._
     val c = sc.wholeTextFiles("/Users/Zealot/yyt-git/SPARK_WB/src/fanstop/rfm/data/").
       filter(_._1.endsWith("fans")).
       flatMap(_._2.split("\n"))
@@ -77,12 +81,27 @@ object FanstopPreprocess {
       val count = x._2._2
       val timestamps = x._2._3
       val aa = getCoreTime(parseDate(timestamps), getNowDate())
-             expo + " " + count + " " + aa
-//      x._1 + "\t" + expo + "\t" + count + "\t" + timestamps
-    }
+             val a = expo + " " + count + " " + aa
+      a.split(" ")
 
-    c.collect.foreach(println)
-println(c.count)
-    c.saveAsTextFile("/Users/Zealot/yyt-git/SPARK_WB/src/fanstop/rfm/trainData/")
+//      x._1 + "\t" + expo + "\t" + count + "\t" + timestamps
+    }.map{x=>
+      RFM(x(0).toInt,x(1).toInt,x(2).toInt)
+    }
+    c.toDF().show(10)
+
+//    new QuantileDiscretizer()
+//      .setInputCol("ages")
+//      .setOutputCol("qdCategory")
+//      .setNumBuckets(4)//设置分箱数
+//      .setRelativeError(0.1)//设置precision-控制相对误差
+//      .fit(df)
+//      .transform(df)
+//      .show(10,false);
+//
+//
+//    c.collect.foreach(println)
+//println(c.count)
+//    c.saveAsTextFile("/Users/Zealot/yyt-git/SPARK_WB/src/fanstop/rfm/trainData/")
   }
 }
