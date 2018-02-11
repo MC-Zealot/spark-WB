fe4d89cb067760744e317a689108fb2bd53ff044
Zealot <yizhou.tao@yinyuetai.com>
2018-02-11 21:42:24 +0800

    修改成可视化的数据

--- src/fanstop/rfm/model/UserClustering.scala
+++ src/fanstop/rfm/model/UserClustering.scala
@@ -1,8 +1,12 @@
 package fanstop.rfm.model
 
+import java.util
+
 import org.apache.spark.mllib.clustering.{KMeans, KMeansModel}
 import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.{SparkContext, SparkConf}
+
+import scala.collection.mutable.ArrayBuffer
 
 /**
  * Created by yizhou on 2018/02/09.
@@ -10,23 +14,54 @@ import org.apache.spark.{SparkContext, SparkConf}
 object UserClustering {
 
   def main(args: Array[String]): Unit = {
-    val sparkConf = new SparkConf().setAppName("UserClustering yizhou").setMaster("local[1]")
+    val sparkConf = new SparkConf().setAppName("UserClustering yizhou").setMaster("local[2]")
     val sc = new SparkContext(sparkConf)
     // Load and parse the data
 //    val data = sc.textFile("/Users/Zealot/dev/spark-2.0.0-bin-hadoop2.4/data/mllib/kmeans_data.txt")
-    val data = sc.textFile("/Users/Zealot/yyt-git/SPARK_WB/src/fanstop/rfm/trainData/")
-    val parsedData = data.map(s => Vectors.dense(s.split(' ').map(_.toDouble))).cache()
+    val data = sc.textFile("/Users/Zealot/yyt-git/SPARK_WB/src/fanstop/rfm/trainData/0211")
+    val parsedData = data.map(s => Vectors.dense(s.split(" ").map(_.toDouble))).cache()
 
     // Cluster the data into two classes using KMeans
-    val numClusters = 8
-    val numIterations = 200
+    val numClusters = 6
+    val numIterations = 30
+
+    //选择err下降比较多的k：6
+//    var a = ArrayBuffer[String]()
+//    val ks:Array[Int] = Array(3,4,5,6,7,8)
+//    ks.foreach(cluster => {
+//      val model:KMeansModel = KMeans.train(parsedData, cluster,20,1)
+//      val ssd = model.computeCost(parsedData)
+//      a+=ssd.toString
+//    })
+//    println(a)
+
+
     val clusters = KMeans.train(parsedData, numClusters, numIterations)
 
     // Evaluate clustering by computing Within Set Sum of Squared Errors
     val WSSSE = clusters.computeCost(parsedData)
     println("Within Set Sum of Squared Errors = " + WSSSE)
     clusters.clusterCenters.foreach(println)
-
+    clusters.predict(parsedData).map(x=>(x,1)).reduceByKey(_+_).sortByKey(false).take(10).foreach(println)
+//    {x:1,y:6,z:5,color:"red"}
+    parsedData.map{x=>
+      val label = clusters.predict(x)
+      var color=""
+      if(label==0){
+        color="red"
+      }else if(label==1){
+        color="yellow"
+      }else if(label==2){
+        color="blue"
+      }else if(label==3){
+        color="green"
+      }else if(label==4){
+        color="purple"
+      }else if(label==5){
+        color="orange"
+      }
+      "{x:"+x(0)+",y:"+x(1)+",z:"+x(2)+",color:\""+color+"\"},"
+    }.repartition(1).distinct.saveAsTextFile("/Users/Zealot/yyt-git/SPARK_WB/src/fanstop/rfm/labeledData/0211_3")
 
     // Save and load model
 //    clusters.save(sc, "target/org/apache/spark/KMeansExample/KMeansModel")
--- src/fanstop/rfm/preprocess/FanstopPreprocess.scala
+++ src/fanstop/rfm/preprocess/FanstopPreprocess.scala
@@ -11,7 +11,8 @@ package fanstop.rfm.preprocess
 import java.text.{DecimalFormat, SimpleDateFormat}
 import java.util.Date
 
-import org.apache.spark.ml.feature.QuantileDiscretizer
+import org.apache.spark.ml.feature.{StandardScaler, QuantileDiscretizer}
+import org.apache.spark.mllib.stat.{Statistics, MultivariateStatisticalSummary}
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.{SparkContext, SparkConf}
 import org.apache.spark.mllib.clustering.{KMeans, KMeansModel}
@@ -34,7 +35,7 @@ object FanstopPreprocess {
     var hehe = dateFormat.format( now )
     hehe
   }
-  case class RFM(r:Int,f:Int,m:Int)
+  case class RFM(r:Double,f:Double,m:Double)//
 
   //核心工作时间，迟到早退等的的处理
   def getCoreTime(start_time: String, end_Time: String) = {
@@ -52,22 +53,20 @@ object FanstopPreprocess {
   def main(args: Array[String]) {
 
 //做一个过滤，粉丝数》50，标签保留人数的top50
-    val sparkConf = new SparkConf().setAppName("FanstopPreprocess yizhou").setMaster("local[1]")
+    val sparkConf = new SparkConf().setAppName("FanstopPreprocess yizhou").setMaster("local[2]")
     val sc = new SparkContext(sparkConf)
     val sqlCon=new SQLContext(sc)
     import sqlCon.implicits._
-    val c = sc.wholeTextFiles("/Users/Zealot/yyt-git/SPARK_WB/src/fanstop/rfm/data/").
-      filter(_._1.endsWith("fans")).
-      flatMap(_._2.split("\n"))
-      .map{x=>
+    val c = sc.wholeTextFiles("file:///Users/Zealot/yyt-git/SPARK_WB/src/fanstop/rfm/data/").
+      filter(_._1.endsWith("all_fans")).
+      flatMap(_._2.split("\n")).map{x=>
       val fields = x.split("\t")
       val uid = fields(1)
       val expo = fields(2)
       val timestamps = fields(3)
 
       (uid, (expo.toInt, 1, timestamps))
-    }
-      .reduceByKey { (x, y) =>
+    }.reduceByKey { (x, y) =>
       var r3 = ""
       if (x._3 < y._3) {
         r3 = y._3
@@ -75,33 +74,60 @@ object FanstopPreprocess {
         r3 = x._3
       }
       (x._1 + y._1, x._2 + y._2, r3)
-    }
-      .map{x=>
+    }.map{x=>
       val expo = x._2._1
       val count = x._2._2
       val timestamps = x._2._3
-      val aa = getCoreTime(parseDate(timestamps), getNowDate())
-             val a = expo + " " + count + " " + aa
+      val recency = getCoreTime(parseDate(timestamps), getNowDate())
+             val a = recency + " " + count + " " + expo
       a.split(" ")
-
-//      x._1 + "\t" + expo + "\t" + count + "\t" + timestamps
     }.map{x=>
-      RFM(x(0).toInt,x(1).toInt,x(2).toInt)
+      RFM(x(0).toDouble,x(1).toDouble,x(2).toDouble)
     }
-    c.toDF().show(10)
+    val cdf = c.toDF()
+//    cdf.show(100)
+//    val mean =cdf.describe("r","f","m").where("summary='mean'")
+//    mean.show()
+//    val r_mean = mean.select("r").collect
+//    val f_mean = mean.select("f").collect
+//    val m_mean = mean.select("m").collect
+//    println(r_mean(0))
+//    println(f_mean(0))
+//    println(m_mean(0))
 
-//    new QuantileDiscretizer()
-//      .setInputCol("ages")
-//      .setOutputCol("qdCategory")
-//      .setNumBuckets(4)//设置分箱数
-//      .setRelativeError(0.1)//设置precision-控制相对误差
-//      .fit(df)
-//      .transform(df)
-//      .show(10,false);
-//
-//
+    val rdf = new QuantileDiscretizer()
+      .setInputCol("r")
+      .setOutputCol("rCategory")
+      .setNumBuckets(5)//设置分箱数
+      .setRelativeError(0.1)//设置precision-控制相对误差
+      .fit(cdf)
+      .transform(cdf);
+
+    val mdf = new QuantileDiscretizer()
+      .setInputCol("m")
+      .setOutputCol("mCategory")
+      .setNumBuckets(5)//设置分箱数
+      .setRelativeError(0.1)//设置precision-控制相对误差
+      .fit(rdf)
+      .transform(rdf);
+
+    val fdf = new QuantileDiscretizer()
+      .setInputCol("f")
+      .setOutputCol("fCategory")
+      .setNumBuckets(3)//设置分箱数
+      .setRelativeError(0.1)//设置precision-控制相对误差
+      .fit(mdf)
+      .transform(mdf)
+//    val cc = fdf.filter("f>1")
+    fdf.show(100)
+    println(fdf.count)
 //    c.collect.foreach(println)
 //println(c.count)
-//    c.saveAsTextFile("/Users/Zealot/yyt-git/SPARK_WB/src/fanstop/rfm/trainData/")
+    fdf.select("rCategory","fCategory","mCategory").rdd.map{x=>
+    val l = x.toString().length
+      x.toString().substring(1,l-1).replace(","," ")
+  }.saveAsTextFile("/Users/Zealot/yyt-git/SPARK_WB/src/fanstop/rfm/trainData/0211")
+
+
   }
 }
